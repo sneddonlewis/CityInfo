f0878b03d1d1c06843e7cfd598cf7b6e0d68a3f5
Lewis Sneddon <lewis@lewiscodes.io>
2022-11-19 18:42:23 +0000

    city entity

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Entities;
+
+public class City
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Name { get; }
+    public string? Description { get; set; }
+
+    public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
+
+    public City(string name)
+    {
+        Name = name;
+    }
+}
