46bb56c16c203e37847fe06d60a62f5533d03c04
Lewis Sneddon <lewis@lewiscodes.io>
2022-11-19 10:43:04 +0000

    points of interest for cities

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -15,13 +15,43 @@ public class CitiesDataStore
             {
                 Id = 1,
                 Name = "New York City",
-                Description = "The one with the big park."
+                Description = "The one with the big park",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id = 1,
+                        Name = "Central Park",
+                        Description = "The most visited urban park"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2,
+                        Name = "Empire State Building",
+                        Description = "A 102 story skyscraper"
+                    },
+                }
             },
             new CityDto()
             {
                 Id = 2,
                 Name = "London",
-                Description = "My city."
+                Description = "My city",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id = 1,
+                        Name = "Liverpool Street Station",
+                        Description = "An entry point to the city"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2,
+                        Name = "The Cheese Grater",
+                        Description = "A skyscraper that looks like a cheese grater"
+                    },
+                }
             },
         };
     }
