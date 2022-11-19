a1e53625fe77166ec393d7f7b003b2a0bcc986dd
Lewis Sneddon <lewis@lewiscodes.io>
2022-11-19 10:25:01 +0000

    get city by id

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -11,4 +11,11 @@ public class CitiesController : ControllerBase
     {
         return new JsonResult(CitiesDataStore.Current.Cities);
     }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+        return new JsonResult(
+            CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+    }
 }
