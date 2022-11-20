70537c30c6223008d09e968585d92a0767aea952
Lewis Sneddon <lewis@lewiscodes.io>
2022-11-20 09:46:49 +0000

    PointOfInterest Description and migration

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -13,6 +13,9 @@ public class PointOfInterest
     [MaxLength(50)]
     public string Name { get; set; }
     
+    [MaxLength(50)]
+    public string Description { get; set; }
+    
     [ForeignKey("CityId")]
     public City? City { get; set; }
     public int CityId { get; set; }
